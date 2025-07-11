7c2ce4109ca41bf32ede1be2d3876723648d2787
Shashika Pathum Dilhara <161570319+PathumDilhara@users.noreply.github.com>
2025-07-11 21:26:33 +0530

    Sample users defining

--- src/main/java/org/appvibessolution/spring_security/config/SecurityConfig.java
+++ src/main/java/org/appvibessolution/spring_security/config/SecurityConfig.java
@@ -2,8 +2,15 @@ package org.appvibessolution.spring_security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration // This annotation indicates that this class contains Spring Security configuration
@@ -17,6 +24,56 @@ public class SecurityConfig {
     //- especially in Spring config classes, it's common and cleaner to just declare throws Exception
     @Bean
     public SecurityFilterChain securityFIlterChain(HttpSecurity http) throws Exception {
-        return http.build();
+
+        // we can do this lambda function same as below
+//        http.csrf(customizer -> customizer.disable());
+//        http.authorizeHttpRequests(request ->
+//                request.anyRequest().authenticated());
+//        http.formLogin(Customizer.withDefaults());
+//        http.httpBasic(Customizer.withDefaults());
+//        http.sessionManagement(session->
+//                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        // Expanded version of the above lambda disabling line
+        // Customizer<CsrfConfigurer<HttpSecurity>> csrfCustomizer = new Customizer<CsrfConfigurer<HttpSecurity>>(){
+        //    @Override
+        //    public void customize(CsrfConfigurer<HttpSecurity> customizer){
+        //       customizer.disable();
+        //    }
+        // };
+        // http.csrf(csrfCustomizer);
+
+//        return http.build();
+
+        // Another way to write the above same code
+        return http
+                .csrf(customizer -> customizer.disable())
+                .authorizeHttpRequests(request ->
+                        request.anyRequest().authenticated())
+                // .formLogin(Customizer.withDefaults()) // ask always if uncommented
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user1 = User
+                .withDefaultPasswordEncoder()
+                .username("user1")
+                .password("123")
+                .roles("USER")
+                .build();
+
+        UserDetails user2 = User
+                .withDefaultPasswordEncoder()
+                .username("user2")
+                .password("456")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
+
     }
 }
